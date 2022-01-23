83dbac4e7d48b5eef56b022050ac6a64dce71950
sunphobic <codywebbear@protonmail.com>
2022-01-23 13:12:13 -0300

    MUI masonty test deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline' //reset css
 import NavBar from './component/NavBar'
 import ListComponent from './component/ListComponent'
-import Prueba from './component/prueba'
+
 
 
 
